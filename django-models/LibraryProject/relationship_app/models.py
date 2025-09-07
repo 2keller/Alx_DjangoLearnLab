3ecab9be5acdefdf0cf12f3a6151f549db21ac3e
2keller <mashayamombekelvin@gmail.com>
2025-09-07 16:22:43 +0200

    Fix typo in Author model import and usage

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 # Create your models here.
-class Aurthur(models.Model):
+class Author(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 class Book(models.Model):
     title = models.CharField(max_length=100)
-    author = models.ForeignKey(Aurthur, on_delete=models.CASCADE, related_name='books')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
 
     def __str__(self):
         return self.title
@@ -29,4 +29,3 @@ class Librian(models.Model):
         return self.name
     
 
-    
--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -9,16 +9,16 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
 django.setup()
 
-from relationship_app.models import Aurthur, Book, Library, Librian
+from relationship_app.models import Author, Book, Library, Librian
 
 def get_books_by_author(author_name):
     try:
-        author = Aurthur.objects.get(name=author_name)
+        author = Author.objects.get(name=author_name)
         books = Book.objects.filter(author=author)
         print(f"Books by {author.name}:")
         for book in books:
             print(book.title)
-    except Aurthur.DoesNotExist:
+    except Author.DoesNotExist:
         print(f"No author found with name '{author_name}'")
 
 def list_books_in_library(library_name):

b38264178b7bb41cd630222def6fb46ad4b0c5e4
2keller <mashayamombekelvin@gmail.com>
2025-09-07 20:52:32 +0200

    fixed all errros

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,17 +4,12 @@ from .views import (
     LibraryDetailView,
     CustomLoginView,
     CustomLogoutView,
-    RegisterView,
+    RegisterView
 )
 
-app_name = 'relationship_app'
-
 urlpatterns = [
-    # Book views
-    path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-
-    # Authentication views
+    path('books/', list_books, name='book-list'),
+    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),

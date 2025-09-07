a756a25caf34057463c058f5a76e59343de61306
2keller <mashayamombekelvin@gmail.com>
2025-09-07 20:26:08 +0200

    Add login and logout URL patterns to urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,9 @@
 #urls 
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView, logout_view, login_view
 urlpatterns = [
     path('books/', list_books, name='book-list'),
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]

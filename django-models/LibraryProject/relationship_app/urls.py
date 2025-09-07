8b641254365c5e650bcb104e8e62fe508a208f96
2keller <mashayamombekelvin@gmail.com>
2025-09-07 17:04:17 +0200

    Add views and URL routing for book and library details

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,7 @@
+#urls 
+from django.urls import path
+from .views import book_list, LibraryDetailView
+urlpatterns = [
+    path('books/', book_list, name='book-list'),
+    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
+]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse, request
+from .models import Book, Author, Library, Librarian
+from django.views import View
 
-# Create your views here.
+
+def book_list(request):
+    books = Book.objects.all()
+    response = ", ".join([f"{book.title} by {book.author}" for book in books])
+    return HttpResponse(response)
+
+
+
+
+
+# Class-based view (no template, just database -> response)
+class LibraryDetailView(View):
+    def get(self, request, pk):
+        library = Library.objects.get(pk=pk)          # Get library by its ID
+        books = library.books.all()                   # All books in that library
+        response = f"Library: {library.name}, Books: " + ", ".join(
+            [f"{book.title} by {book.author}" for book in books]
+        )
+        return HttpResponse(response)
+
+    

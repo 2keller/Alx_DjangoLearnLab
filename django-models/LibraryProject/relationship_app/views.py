9ff6dc6ded82581e220e27e497ec78050657242d
2keller <mashayamombekelvin@gmail.com>
2025-09-07 17:22:20 +0200

    Refactor LibraryDetailView to use Django's DetailView for improved functionality and clarity

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, request
 from .models import Book, Author, Library, Librarian
 from django.views import View
+from django.views.generic.detail import DetailView
 
 
 def list_books(request):
@@ -16,13 +17,12 @@ def list_books(request):
 
 
 # Class-based view (no template, just database -> response)
-class LibraryDetailView(View):
-    def get(self, request, pk):
-        library = Library.objects.get(pk=pk)          # Get library by its ID
-        books = library.books.all()                   # All books in that library
-        response = f"Library: {library.name}, Books: " + ", ".join(
-            [f"{book.title} by {book.author}" for book in books]
-        )
-        return render(request, 'library_detail.html', {'library': library, 'books': books})
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+    def get_object(self):
+        return get_object_or_404(Library, pk=self.kwargs['pk'])
 
     

e718b12f09bbbe317a063f57465890a300dc302d
2keller <mashayamombekelvin@gmail.com>
2025-09-13 23:48:10 +0200

    fixed errors with book in views

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 
 @permission_required('advanced_features_and_security.can_edit', raise_exception=True)
-def edit_article(request, article_id):
-    # your edit logic here
-    return render(request, 'edit_article.html', {'article_id': article_id})
+def book_list(request, book):
+    book.all()
+    return render(request, 'bookshelf/book_list.html', {})

1010e3f97e28a15ec5cf2c98baaf5ae5f04cd681
2keller <mashayamombekelvin@gmail.com>
2025-09-14 01:02:33 +0200

    fixed views to include forms import

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from forms import ExampleForm
 from django.contrib.auth.decorators import permission_required
 
 @permission_required('advanced_features_and_security.can_edit', raise_exception=True)

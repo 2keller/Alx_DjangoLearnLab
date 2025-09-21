167440e96a7e477983172bb81db75124b125f642
2keller <mashayamombekelvin@gmail.com>
2025-09-21 19:57:50 +0200

    done with my first full stack app well not really done but yay

--- log/simple_log/urls.py
+++ log/simple_log/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from logs import views  # Add this import
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('logs/', include('logs.urls')),
+    path('', views.home, name='home'),  # Add this line
 ]
+# ...existing code...

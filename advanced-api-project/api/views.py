c628bf3116827725d5aa7e5c9bc04a8e146edc0d
2keller <mashayamombekelvin@gmail.com>
2025-09-26 19:48:07 +0200

    added the views and the urls for the views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+from .models import Book
+from django.urls import reverse_lazy
+from django.views.generic  import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .serializers import BookSerializer, AuthorSerializer
 # Create your views here.
+#implimenting crud operations fro the book using views 
+
+class BookListView(ListView):
+    model = Book
+    
+class BookDetailView(DetailView):
+    model = Book
+
+class BookCreateView(CreateView):
+    model = Book
+    fields = '__all__'
+    success_url = reverse_lazy('book-list')
+
+class BookUpdateView(UpdateView):
+    model = Book
+    fields = '__all__'
+    success_url = reverse_lazy('book-list')
+
+class BookDeleteView(DeleteView):
+    model = Book
+    success_url = reverse_lazy('book-list')
+
+    

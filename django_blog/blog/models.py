c1d65fb6897c81158138acfbc11be54593235da1
2keller <mashayamombekelvin@gmail.com>
2025-09-30 21:58:09 +0200

    added the users module in my models still trying to understand why

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,17 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Author(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField()
 
     def __str__(self):
-        return self.name
-    
+        return self.user.username
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     published_date = models.DateTimeField(auto_now_add=True)
-    
+
                  

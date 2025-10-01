cec37524a222b89857089f391a0141057707fc49
2keller <mashayamombekelvin@gmail.com>
2025-10-01 21:13:08 +0200

    created the templates, added the urls and created the views

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from . models import Author
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
-# Create your views here.
+class CustomUSerCReationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+#a view to handle registration of new users to the site 
+
+def Register(request):
+    if request.method == "POST":
+        form = CustomUSerCReationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = CustomUSerCReationForm()
+    return render(request, "registration/register.html", {"form": form})
+
+@login_required
+def profile(request):
+    return render(request, "registration/profile.html")

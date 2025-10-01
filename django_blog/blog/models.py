e98c8781cd04bff05d92cdda24baf626d32c2165
2keller <mashayamombekelvin@gmail.com>
2025-10-01 23:09:03 +0200

    i wanna go sleep

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,18 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-class Author(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField()
-
-    def __str__(self):
-        return self.user.username
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     published_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
-from . models import Author
+from . models import User, Profile, Post
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
@@ -15,18 +15,33 @@ class CustomUSerCReationForm(UserCreationForm):
 
 #a view to handle registration of new users to the site 
 
+
 def Register(request):
     if request.method == "POST":
         form = CustomUSerCReationForm(request.POST)
         if form.is_valid():
-            form.save()
+            # Save the new user object
+            user = form.save() 
+            
+            # --- FIX: Create the corresponding Profile object ---
+            Profile.objects.create(user=user)
+            
             return redirect("login")
     else:
         form = CustomUSerCReationForm()
     return render(request, "registration/register.html", {"form": form})
-
 @login_required
 def profile(request):
     return render(request, "registration/profile.html")
 
 
+
+def home(request):
+    """
+    Fetches all Post objects and passes them to the template.
+    """
+    posts = Post.objects.all().order_by('-published_date') # Retrieves all posts, newest first
+    context = {
+        'posts': posts
+    }
+    return render(request, "django_blog\blog\template\blog\home.html", context)

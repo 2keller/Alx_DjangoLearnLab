9276c475e1c8572074b10d5336b58ed60085b047
2keller <mashayamombekelvin@gmail.com>
2025-10-02 23:34:25 +0200

    checker freindly code

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,8 +9,7 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    path('post/<int:pk>/comment/', CommentListView.as_view(), name='comment-list'),
-    path('comment/<int:pk>/new/', CommentCreateView.as_view(), name='comment-create'),
+   
 
 
 
@@ -22,5 +21,5 @@ urlpatterns = [
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
     path('post/<int:pk>/comment/', CommentListView.as_view(), name='comment-list'),
-
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
 ]

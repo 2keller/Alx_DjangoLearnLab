8e4c395342ee024b9fa7872a41070a981aa113a6
2keller <mashayamombekelvin@gmail.com>
2025-10-09 22:58:03 +0200

    added urls

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
+    path('follow/<str:username>/', views.follow_user, name='follow_user'),
+    path('unfollow/<str:username>/', views.unfollow_user, name='unfollow_user'),
 ]

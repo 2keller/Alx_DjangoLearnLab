ff44ec2a089ff365e19148e5d0e3cb594618b9b0
2keller <mashayamombekelvin@gmail.com>
2025-10-09 13:49:11 +0200

    added nlogin anfd registration views

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,7 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import CustomUser
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import RegisterSerializer, LoginSerializer
+from rest_framework.permissions import AllowAny
+
 
 # Create your views here.
 #create account and login logic 
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({
+                'message': 'User registered successfully',
+                'username': user.username,
+                'email': user.email,
+                'token': user.auth_token.key
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            token = serializer.validated_data['token']
+            return Response({
+                'message': 'Login successful',
+                'username': user.username,
+                'token': token
+            }, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
